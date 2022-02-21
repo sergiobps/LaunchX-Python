6c5b615c2d2b504ebd3b7169848ff5e13d09dc25
Sergio Ortega <sergiobps@live.com>
2022-02-21 03:38:12 -0600

    Initial commit

---
+++ Modulo_10/open.py
@@ -0,0 +1,37 @@
+#def main():
+#    open("/path/to/mars.jpg")
+#    
+    
+#def main():
+#    try:
+#        configuration = open('config.txt')
+#    except FileNotFoundError:
+#        print("Couldn't find the config.txt file!")
+
+    
+#def main():
+#    try:
+#        configuration = open('config.txt')
+#    except Exception:
+#        print("Couldn't find the config.txt file!")
+        
+#def main():
+#    try:
+#        configuration = open('config.txt')
+#    except FileNotFoundError:
+#        print("Couldn't find the config.txt file!")
+#    except IsADirectoryError:
+#        print("Found config.txt but it is a directory, couldn't read it")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+        
+if __name__ == '__main__':
+    main()
